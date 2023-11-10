bca9125b3faef77e7f08ad286d714de428d2174a
mehdi <gudarzi.gi@gmail.com>
2023-11-10 11:15:09 +0330

    final

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace game
+{
+    partial class fm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labEnd = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labEnd
+            // 
+            this.labEnd.BackColor = System.Drawing.Color.Green;
+            this.labEnd.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labEnd.Location = new System.Drawing.Point(-9, 63);
+            this.labEnd.Name = "labEnd";
+            this.labEnd.Size = new System.Drawing.Size(99, 26);
+            this.labEnd.TabIndex = 7;
+            this.labEnd.Text = "پایان";
+            this.labEnd.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labEnd.MouseEnter += new System.EventHandler(this.labEnd_MouseEvent);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Yellow;
+            this.label2.Location = new System.Drawing.Point(240, -5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 57);
+            this.label2.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Yellow;
+            this.label3.Location = new System.Drawing.Point(267, -5);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 26);
+            this.label3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Yellow;
+            this.label4.Location = new System.Drawing.Point(172, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 26);
+            this.label4.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Yellow;
+            this.label5.Location = new System.Drawing.Point(153, 37);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 52);
+            this.label5.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Yellow;
+            this.label6.Location = new System.Drawing.Point(80, 63);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 26);
+            this.label6.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(361, -5);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "شروع";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // fm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(427, 121);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labEnd);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "fm";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "بازی";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.fm_KeyDown);
+            this.MouseEnter += new System.EventHandler(this.fm_mouseEvent);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label labEnd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace game
+{
+    public partial class fm : Form
+    {
+
+        private bool play = false;
+        public fm()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.play = true;
+           
+
+        }
+
+        private void fm_mouseEvent(object sender, EventArgs e) {
+           
+            if (this.play)
+            {
+                MessageBox.Show("بازی رو باختی دوباره شروع کن  ");
+                this.play = false;
+            }
+           
+           
+
+        }
+
+        private void labEnd_MouseEvent(object sender, EventArgs e)
+        {
+            if (this.play)
+            {
+                MessageBox.Show("بردی :D");
+                this.play = false;
+            }
+           
+        }
+
+        private void fm_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Enter)
+            {
+
+                button1_Click(sender, e);
+
+
+            }
+            else if (e.KeyCode == Keys.Escape)
+            { 
+                
+               if (!this.play)
+                {
+                    MessageBox.Show("بازی رو خاتمه دادید !");
+                    this.play = false;
+
+                }
+            
+            
+            }
+
+        }
+
+
+    }
+}
